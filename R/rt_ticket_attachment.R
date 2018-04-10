71b28d211975dd0fd9732eb0877dd4d3da08bd6a
Irene <25118334+isteves@users.noreply.github.com>
2018-04-10 14:58:36 -0700

    added documentation

--- R/rt_ticket_attachment.R
+++ R/rt_ticket_attachment.R
@@ -1,3 +1,21 @@
+#' Get ticket attachment
+#'
+#' Retrieves attachment information using the GET method.
+#'
+#' @param base 
+#' @param ticket 
+#' @param attachment 
+#'
+#' @return
+#' @export
+#' 
+#' @importFrom httr GET
+#'
+#' @examples
+#' \dontrun{
+#' rt_ticket_attachment("https://server.name/rt/", "12345", "56789)
+#' }
+
 rt_ticket_attachment <- function(base, ticket, attachment) {
   if (missing(ticket)) {
     stop("The argument 'ticket' must be specified.", call. = FALSE)
--- R/rt_ticket_attachments.R
+++ R/rt_ticket_attachments.R
@@ -1,3 +1,20 @@
+#' Get ticket attachments
+#'
+#' Retrieves attachment metadata using the GET method.
+#'
+#' @param base 
+#' @param ticket 
+#'
+#' @return
+#' @export
+#' 
+#' @importFrom httr GET
+#'
+#' @examples
+#' \dontrun{
+#' rt_ticket_attachments("https://server.name/rt/", "12345")
+#' }
+
 rt_ticket_attachments <- function(base, ticket) {
   if (missing(ticket)) {
     stop("The argument 'ticket' must be specified.", call. = FALSE)
