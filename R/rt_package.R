2ab5a4fd58f4c5ea7db0dab36eee7082eaabba95
Irene <jane@example.com>
2018-06-11 10:18:31 -0700

    Added rt package documentation; imports httr directly and sets globalVariables

---
+++ R/rt_package.R
@@ -0,0 +1,17 @@
+#' \code{rt} package
+#'
+#' Request Tracker R API
+#'
+#' See \href{https://github.com/NCEAS/rt}{GitHub}
+#'
+#' @docType package
+#' @name rt
+#' @importFrom dplyr %>%
+#' @import httr
+#'
+NULL
+
+if(getRversion() >= "2.1.15") {
+  utils::globalVariables(c(".",
+                           "colname", "value")) # for search
+}
