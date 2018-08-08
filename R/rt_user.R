8a3e5365c9c5f8f72b82d22e1016856419044bf5
Bryce Mecum <petridish@gmail.com>
2018-08-08 00:15:32 -0800

    Remove _properties suffix from a few calls/functions

--- R/rt_user.R
+++ R/rt_user.R
@@ -13,7 +13,7 @@
 #' rt_user_properties(1)
 #' }
 
-rt_user_properties <- function(user_id, rt_base = getOption("rt_base")) {
+rt_user <- function(user_id, rt_base = getOption("rt_base")) {
   stopifnot(is.character(user_id) | is.numeric(user_id))
 
   url <- rt_url(rt_base, "user", user_id)
