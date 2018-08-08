1b62a007d75beffa7474b1eaeb8d5bda42e4e510
Bryce Mecum <petridish@gmail.com>
2018-08-08 00:16:58 -0800

    Make some high level edits to docstrings, formatting, etc

--- R/rt_ticket_search.R
+++ R/rt_ticket_search.R
@@ -1,8 +1,8 @@
 #' Search RT
 #'
-#' Search RT for tickets that fit your query.
+#' Search RT for tickets using RT's query syntax. Queries are specified using RT's custom syntax which you can read about at \url{https://docs.bestpractical.com/rt/4.4.3/query_builder.html}.
 #'
-#' @param query (character) A query
+#' @param query (character) Your query (See Details)
 #' @param orderBy (character) How to order your search results
 #' @param format (character) Either \code{i} (ticket ID only),
 #' \code{s} (ticket ID and subject), or \code{l} (full ticket metadata).
@@ -11,15 +11,10 @@
 #'
 #' @export
 #'
-#' @importFrom tidyr unnest separate spread
-#' @import dplyr
-#' @importFrom tibble tibble
-#' @import stringr
-#'
 #' @examples
 #' \dontrun{
 #' # To return all un-owned tickets on a queue:
-#' rt_ticket_search(query = "Queue='General'AND(Status='new')")
+#' rt_ticket_search(query = "Queue='General' AND (Status='new')")
 #' }
 
 rt_ticket_search <- function(query, orderBy = NULL, format="l", rt_base = getOption("rt_base")) {
@@ -32,37 +27,18 @@ rt_ticket_search <- function(query, orderBy = NULL, format="l", rt_base = getOpt
                          format = format))
 
   params <- paste(paste0(names(inputs), "=", inputs), collapse = "&")
-
   url <- paste0(base_api, params)
-
   out <- rt_GET(url)
 
-  if(format == "s"){
+  if (format == "s") {
     out$content <- out$content %>%
       tidyr::gather(id, value)
   }
 
-  if(format == "i"){
+  if (format == "i") {
     out$content <- out$content %>%
       stringr::str_split("\n")
   }
 
   return(out)
-
-  # if (stringr::str_detect(httr::content(req), "Bad request")) {
-  #   stop(httr::content(req), call. = FALSE)
-  # }
-  #
-  # if (format != "l") {
-  #   return(req)
-  # } else {
-  #   result <- tibble::tibble(content = stringr::str_split(httr::content(req), "\\n--\\n")[[1]]) %>%
-  #     mutate(line = 1:n(),
-  #            content = lapply(content, tabularize_ticket)) %>%
-  #     tidyr::unnest() %>%
-  #     tidyr::spread(ticket_headers, ticket_content) %>%
-  #     dplyr::select_if(function(column) {!all(column == "" | is.na(column))}) #not empty
-  #
-  #   return(result)
-  # }
 }
