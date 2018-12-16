62286377fddcc40c2085ed141ae545dbdc1e7027
Bryce Mecum <petridish@gmail.com>
2018-12-15 20:51:50 -0500

    Add some tests for rt_url

---
+++ tests/testthat/test_rt_url.R
@@ -0,0 +1,25 @@
+context("rt_url")
+
+test_that("can construct URLs", {
+  expect_equal(rt_url("a"),
+               "http://localhost:8080/REST/1.0/a")
+  expect_equal(rt_url("a", "b"),
+               "http://localhost:8080/REST/1.0/a/b")
+  expect_equal(rt_url("a", "b", 3),
+               "http://localhost:8080/REST/1.0/a/b/3")
+})
+
+test_that("can construct URLs with query parameters", {
+  expect_equal(rt_url("asdf", query_params = list(param = "foo")),
+               "http://localhost:8080/REST/1.0/asdf?param=foo")
+  expect_equal(rt_url("asdf", query_params = list(param = "foo",
+                                                    other_param = "bar")),
+               "http://localhost:8080/REST/1.0/asdf?param=foo&other_param=bar")
+})
+
+test_that("removes NULL items from query params", {
+  expect_equal(rt_url("asdf", query_params = list(param = NULL)),
+               "http://localhost:8080/REST/1.0/asdf")
+  expect_equal(rt_url("asdf", query_params = list(param = 1, other = NULL)),
+               "http://localhost:8080/REST/1.0/asdf?param=1")
+})
