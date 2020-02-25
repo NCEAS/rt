d8443d3b985e53ce7d3e1f3ab8e8e8c6a5c497d1
Bryce Mecum <petridish@gmail.com>
2020-02-24 22:48:59 -0900

    Refactor search to try tabularizing results

--- R/rt_ticket_search.R
+++ R/rt_ticket_search.R
@@ -1,6 +1,38 @@
+#' tidy_long_search_result
+#'
+#' @param result (list) List of lists from search results
+#' @param coerce_tibble (logical) Whether or not to try to coerce the result to
+#' a `tibble`.
+#'
+#' @return A `data.frame` or `tbl_df`
+tidy_long_search_result <- function(result, coerce_tibble = TRUE) {
+  # Turn into a list of data.frames with cleaner names
+  dfs <- lapply(result, function(r) {
+    rdf <- data.frame(t(unlist(r)), stringsAsFactors = FALSE)
+
+    # Remove spaces and periods from names
+    names(rdf) <- gsub("[ \\.]*", "", names(rdf))
+
+    rdf
+  })
+
+  all_dfs <- do.call(rbind.data.frame, dfs)
+
+  # Remove "ticket/" from `id` column since ids should always be numbers
+  all_dfs$id <- gsub("ticket/", "", all_dfs$id)
+
+  # Coerce to tibble if appropriate
+  if (coerce_tibble && requireNamespace("tibble")) {
+    all_dfs <- tibble::as_tibble(all_dfs)
+  }
+
+  all_dfs
+}
+
 #' Search RT
 #'
-#' Search RT for tickets using RT's query syntax. Queries are specified using RT's custom syntax which you can read about at \url{https://docs.bestpractical.com/rt/4.4.3/query_builder.html}.
+#' Search RT for tickets using RT's query syntax which is documented at
+#' \url{https://docs.bestpractical.com/rt/4.4.3/query_builder.html}.
 #'
 #' @param query (character) Your query (See Details)
 #' @param orderby (character) How to order your search results. Should be a
@@ -10,7 +42,13 @@
 #' Defaults to \code{l}.
 #' @param fields (character) Comma-separated list of fields to include in the
 #' results.
+#' @param coerce_tibble (logical) Whether or not to make the result when
+#' `format` is `l` or `s` into a `tibble`. Defaults to `TRUE` and only works
+#' when the `tibble` package is installed.
 #' @param ... Other arguments passed to \code{\link{rt_GET}}
+#'
+#' @return Either a data.frame/tibble (when format is `l` or `s`) or a numeric
+#' vector when its `i`.
 #'
 #' @export
 #'
@@ -18,13 +56,21 @@
 #' \dontrun{
 #' # To return all un-owned tickets on a queue:
 #' rt_ticket_search("Queue='General' AND (Status='new')")
+#'
+#' # We can sort by date created, increasing
 #' rt_ticket_search("Queue='General' AND (Status='new')",
-#'                  orderBy = "+Created")
+#'                  orderby = "+Created")
+#'
+#' # If we just need a vector of ticket ids
+#' rt_ticket_search("Queue='General' AND (Status='new')",
+#'                  orderby = "+Created",
+#'                  format = "i")
 #' }
 rt_ticket_search <- function(query,
                              orderby = NULL,
-                             format="l",
+                             format = "l",
                              fields = NULL,
+                             coerce_tibble = TRUE,
                              ...) {
   if (!(format %in% c("l", "s", "i"))) {
       stop(call. = FALSE,
@@ -50,12 +96,24 @@ rt_ticket_search <- function(query,
     result <- lapply(
       strsplit(response$body, "\\n")[[1]],
       parse_rt_properties)
+
+    result <- data.frame(id = unlist(lapply(result, names)),
+                         Subject = unlist(lapply(result, unlist, use.names = FALSE)),
+                         stringsAsFactors = FALSE)
+    names(result) <- c("id", "Subject")
+
+    if (coerce_tibble && requireNamespace("tibble")) {
+      result <- tibble::as_tibble(result)
+    }
   } else if (format == "i") {
     result <- stringr::str_split(response$body, "\n")[[1]]
+    result <- gsub("ticket/", "", result) # Remove "ticket/"
   } else if (format == "l" ) {
     result <- lapply(
       strsplit(response$body, "\\n\\n--\\n\\n")[[1]],
       parse_rt_properties)
+
+    result <- tidy_long_search_result(result, coerce_tibble)
   }
 
   result
