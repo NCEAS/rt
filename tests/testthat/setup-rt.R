eabbb0c34f7f43bc47df32bc76c645cf8464c9e4
Bryce Mecum <petridish@gmail.com>
2020-02-26 10:50:59 -0900

    Update test setup file for new CI setup

--- tests/testthat/setup-rt.R
+++ tests/testthat/setup-rt.R
@@ -1,5 +1,18 @@
-# Log us in if we're not on CRAN
-if (!testthat:::on_cran()) {
-  Sys.setenv(RT_BASE_URL = "http://localhost")
-  rt_login("root", "password")
+#' The test suite runs only unit tests by default.
+#' Most of the useful tests are integration tests that rely on having a test
+#' instance of RT to make requests to.
+#'
+#' For ease of use, a Docker run command is listed in the README for launching
+#' a local instance of RT and the test suite is configured to make requests
+#' to it.
+#'
+#' To all of the tests to run, uncomment the next line:
+# Sys.setenv("RT_INTEGRATION" = TRUE)
+
+# Log us in if we're not on CRAN and we're running integration tests
+if (!testthat:::on_cran() && Sys.getenv("RT_INTEGRATION") == TRUE) {
+  Sys.setenv(RT_BASE_URL = "http://localhost",
+             RT_USER = "root",
+             RT_PASSWORD = "password")
+  rt_login()
 }
